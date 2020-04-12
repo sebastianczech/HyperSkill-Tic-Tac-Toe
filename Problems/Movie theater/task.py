44d4f3083bde7f9a422b141b2c0bd2966d9dee30
Sebastian Czech <sebaczech@gmail.com>
2020-04-12 22:22:51 +0200

    step: Comparisons

---
+++ Problems/Movie theater/task.py
@@ -0,0 +1,6 @@
+# put your python code here
+n = int(input())
+k = int(input())
+v = int(input())
+
+print(n * k >= v)
---
+++ Problems/Very odd/task.py
@@ -0,0 +1,9 @@
+# put your python code here
+a = int(input())
+b = int(input())
+
+# a = 10000000000
+# b = 5
+r = a / b
+# print((a / b) > 0 and (a % b) == 0)
+print(r % 2 == 1)
