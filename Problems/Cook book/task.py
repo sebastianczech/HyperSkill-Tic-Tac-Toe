0d0576e0c937c532e9874bdfe6ceb68165ce3de1
Sebastian Czech <sebaczech@gmail.com>
2020-04-13 22:43:31 +0200

    step: If

---
+++ Problems/Cook book/task.py
@@ -0,0 +1,18 @@
+pasta = "tomato, basil, garlic, salt, pasta, olive oil"
+apple_pie = "apple, sugar, salt, cinnamon, flour, egg, butter"
+ratatouille = "aubergine, carrot, onion, tomato, garlic, olive oil, pepper, salt"
+chocolate_cake = "chocolate, sugar, salt, flour, coffee, butter"
+omelette = "egg, milk, bacon, tomato, salt, pepper"
+
+ingridient = input()
+
+if ingridient in pasta:
+    print("You can make pasta")
+if ingridient in apple_pie:
+    print("You can make apple pie")
+if ingridient in ratatouille:
+    print("You can make ratatouille")
+if ingridient in chocolate_cake:
+    print("You can make chocolate cake")
+if ingridient in omelette:
+    print("You can make omelette")
